40e60cee11aa532c4c60005865b80f42d0c7f9a3
JATIN KAUSHIK <149110777+jatinkaushik052@users.noreply.github.com>
2024-04-07 09:56:56 -0700

    Update item.js

--- item.js
+++ item.js
@@ -1,44 +1,44 @@
 let items = [
     {   id:'001',
         item_Discount: 'Starting ₹99| Start your fitness journey',
-        item_image: 'https://images-eu.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/G/31/img19/Sports/GW_Desktop/1199101_379x304_Compressed._SY304_CB448278349_.jpg',
-        item_Price: 606
-    },
+        item_image: './img/fitness.jpg',
+        item_Price: '₹606'
+    }, 
     {
         id:'002',
         item_Discount: 'Up to 60% off | Car, bike parts & accessories',
-        item_image: 'https://images-eu.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/G/31/2023/GateWay/December/CC/PC_379x304_1._SY304_CB572341311_.jpg',
-        item_Price: 450
+        item_image: './img/automobile.jpg',
+        item_Price: '₹456'
     },
     {
         id:'003',
-        item_Discount: 'Womens Polyester Jacket',
-        item_image: 'https://m.media-amazon.com/images/I/41QLoOP2mSL._SY445_SX342_.jpg',
-        item_Price: 550
+        item_Discount: 'Customers’ Most-Loved Fashion for you',
+        item_image: './img/girl.jpg',
+        item_Price: '₹550'
     },
     {
         id:'004',
-        item_Discount: 'Starting ₹99 | Indoor plants & more',
-        item_image: 'https://images-eu.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/G/31/img18/Lawn_Garden/Ud/MSO_May/compressed_379x304_compressed._SY304_CB592193370_.jpg',
-        item_Price: 450
+        item_Discount: 'Under ₹699 | Combo packs | Amazon brands & more',
+        item_image: './img/tshirt.jpg',
+        item_Price: '₹456'
     },
     {
         id:'005',
-        item_Discount: 'Women Style Faux  Handbag',
-        item_image: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61Fa7wW9FvL._AC_UL320_.jpg',
-        item_Price: 750
+        item_Discount: 'Up to 50% off | International brands',
+        item_image: './img/nike.jpg',
+        item_Price: '₹756'
     },
     {
         id:'006',
-        item_Discount: 'Maybelline New York Lipstick',
-        item_image: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51jD170ZkGL._AC_UL320_.jpg',
-        item_Price: 150
+        item_Discount: 'Men Shport Shoes',
+        item_image: './img/shoes.jpg',
+        item_Price: '₹756'
     },
     {
         id:'006',
-        item_Discount: 'Up to 60% off | Car, bike parts & accessories',
-        item_image: 'https://images-eu.ssl-images-amazon.com/images/W/MEDIAX_792452-T2/images/G/31/2023/GateWay/December/CC/PC_379x304_1._SY304_CB572341311_.jpg',
-        item_Price: 450
+        item_Discount: "Bestsellers in Women's Indian Clothing",
+        item_image: './img/cloth.jpg',
+        item_Price: '₹356'
     },
 
 ]
